df040643703ba4c11a7c65fc6fbbde8a1380d22e
Alberto Giunta <alb.giunta@gmail.com>
2015-02-21 11:22:52 +0100

    pickers ok

--- app/src/main/java/com/example/lisamazzini/train_app/GUI/Fragment/pickers/TimePickerFragment.java
+++ app/src/main/java/com/example/lisamazzini/train_app/GUI/Fragment/pickers/TimePickerFragment.java
@@ -1,4 +1,4 @@
-package com.example.lisamazzini.train_app.gui.fragment.dateTimePickers;
+package com.example.lisamazzini.train_app.gui.fragment.pickers;
 
 import android.app.Dialog;
 import android.app.TimePickerDialog;
@@ -6,20 +6,23 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.format.DateFormat;
 
-public class TimePickerFragment extends DialogFragment implements IPicker {
+/**
+ * Fragment che permette di visualizzare un time picker.
+ *
+ * @author albertogiunta
+ */
+public class TimePickerFragment extends DialogFragment implements IPicker<TimePickerDialog.OnTimeSetListener> {
 
     private TimePickerDialog.OnTimeSetListener timeListener;
     private int hour, minute;
 
-    public TimePickerFragment() {
-    }
-
-    public void setCallback(final TimePickerDialog.OnTimeSetListener timeListener) {
-        this.timeListener = timeListener;
+    @Override
+    public final void setCallback(final TimePickerDialog.OnTimeSetListener pListener) {
+        this.timeListener = pListener;
     }
 
     @Override
-    public void setArguments(final Bundle args) {
+    public final void setArguments(final Bundle args) {
         super.setArguments(args);
         this.hour = args.getInt("hour");
         this.minute = args.getInt("minute");
@@ -27,7 +30,7 @@ public class TimePickerFragment extends DialogFragment implements IPicker {
     }
 
     @Override
-    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+    public final Dialog onCreateDialog(final Bundle savedInstanceState) {
         return new TimePickerDialog(getActivity(), timeListener, hour, minute, DateFormat.is24HourFormat(getActivity()));
     }
 }
